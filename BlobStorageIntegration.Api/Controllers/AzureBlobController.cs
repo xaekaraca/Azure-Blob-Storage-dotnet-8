ff97355616242dfb1fe89869d4ce31e3197ae6f6
Ahmet Emre Karaca <ahmetemre_k@hotmail.com>
2023-10-15 00:17:30 +0300

    Unused namespaces removed

--- BlobStorageIntegration.Api/Controllers/AzureBlobController.cs
+++ BlobStorageIntegration.Api/Controllers/AzureBlobController.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Security.Principal;
 using BlobStorageIntegration.Api.Models;
 using BlobStorageIntegration.Api.Services;
 using Microsoft.AspNetCore.Mvc;
